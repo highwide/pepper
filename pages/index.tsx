b2f8b785df98cf4ea6b3dfc46b87089d14367909
highwide <hochweit728@gmail.com>
2021-05-22 23:51:40 +0900

    Call me "Pepper"

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>呼称表ジェネレーター</h1>
+  <Layout title="Pepper">
+    <h1>Pepper</h1>
     <p>
       <Link href="/koshohyo/new">
         <a>作る</a>
